b3a8672835284d0065e4eae2329757090cd5360e
Raphael Ferreira de Moraes <raphaelfm.2012@gmail.com>
2022-11-30 18:08:54 -0300

    Define variáveis

--- Program.cs
+++ Program.cs
@@ -6,15 +6,15 @@ class Program
     {
         EnviaEmail email = new EnviaEmail();
         System.Console.WriteLine("Digite o seu email: ");
-        var seuEmail = Console.ReadLine();
+        string seuEmail = Console.ReadLine();
         System.Console.WriteLine("Digite a senha do seu email: ");
-        var senhaEmail = Console.ReadLine();
+        string senhaEmail = Console.ReadLine();
         System.Console.WriteLine("Digite o email do destinatário: ");
-        var emailDestinatario = Console.ReadLine();
+        string emailDestinatario = Console.ReadLine();
         System.Console.WriteLine("Digite o Assunto do email: ");
-        var assunto = Console.ReadLine();
+        string assunto = Console.ReadLine();
         System.Console.WriteLine("Digite a mensagem: ");
-        var mensagem = Console.ReadLine();
+        string mensagem = Console.ReadLine();
         Console.WriteLine("Enviando email");
         email.Email(assunto, mensagem, seuEmail,senhaEmail, emailDestinatario);
     }
