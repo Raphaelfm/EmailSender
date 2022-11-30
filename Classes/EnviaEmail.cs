98e8c5c125c389a555a2e4fe04d8ca8acf693b7f
Raphael Ferreira de Moraes <raphaelfm.2012@gmail.com>
2022-11-30 18:06:29 -0300

    Corrige espaço

--- Classes/EnviaEmail.cs
+++ Classes/EnviaEmail.cs
@@ -25,7 +25,6 @@ namespace EmailSender.Classes
                     smtpMessage.IsBodyHtml = false;
                     smtp.Send(smtpMessage);
                 }
-
             }
             catch (Exception ex)
             {
