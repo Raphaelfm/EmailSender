24db37d4b445e08c9d692903dbfe973103400ce9
Raphael Ferreira de Moraes <raphaelfm.2012@gmail.com>
2022-11-30 18:04:10 -0300

    First commit

---
+++ Classes/EnviaEmail.cs
@@ -0,0 +1,38 @@
+using System.Net;
+using System.Net.Mail;
+
+namespace EmailSender.Classes
+{
+    public class EnviaEmail
+    {
+        public void Email(string assunto, string mensagem, string seuEmail, string senhaEmail, string emailDestinatario)
+        {
+            try
+            {
+                var smtp = new SmtpClient
+                {
+                    Host = "SMTP.office365.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(seuEmail, senhaEmail)
+                };
+                using (var smtpMessage = new MailMessage(seuEmail, emailDestinatario))
+                {
+                    smtpMessage.Subject = assunto;
+                    smtpMessage.Body = mensagem;
+                    smtpMessage.IsBodyHtml = false;
+                    smtp.Send(smtpMessage);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //todo: add logging integration
+                //throw;
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EmailSender;
+using Classes;
+class Program
+{
+    static void Main(string[] args)
+    {
+        EnviaEmail email = new EnviaEmail();
+        System.Console.WriteLine("Digite o seu email: ");
+        var seuEmail = Console.ReadLine();
+        System.Console.WriteLine("Digite a senha do seu email: ");
+        var senhaEmail = Console.ReadLine();
+        System.Console.WriteLine("Digite o email do destinatário: ");
+        var emailDestinatario = Console.ReadLine();
+        System.Console.WriteLine("Digite o Assunto do email: ");
+        var assunto = Console.ReadLine();
+        System.Console.WriteLine("Digite a mensagem: ");
+        var mensagem = Console.ReadLine();
+        Console.WriteLine("Enviando email");
+        email.Email(assunto, mensagem, seuEmail,senhaEmail, emailDestinatario);
+    }
+}
